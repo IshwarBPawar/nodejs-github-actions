eaea7830bf6e70bf2df0edfbeab785de4e8220f8
Ishwar Pawar <97723377+IshwarBPawar@users.noreply.github.com>
2026-02-24 16:20:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
- res.send("Hello from GitHub Actions CI/CD 🚀");
+ res.send("Hello from GitHub Actions CI/CD updated code to check cicd 🚀");
 });
 
 app.listen(3000, () => {
