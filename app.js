28b7dfca55368142cb17e57c617712149d840d1e
Ishwar Pawar <97723377+IshwarBPawar@users.noreply.github.com>
2026-02-25 14:43:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
- res.send("Hello from GitHub Actions CI/CD updated code to check cicd with slef-hosted 🚀");
+ res.send("Hello devops🚀");
 });
 
 app.listen(3000, () => {
